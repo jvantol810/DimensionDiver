985d53fad7f01728bc900372efab9f0850fe607f
jvantol810 <63761346+jvantol810@users.noreply.github.com>
2021-06-17 15:15:30 -0400

    Removed useDefaultOptions from player movement script and set the default values in the variable declaration. Increased default player speed and altered movement to take speed into account.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     //Public Variables for customization
-    public bool useDefaultOptions; //If this is true, then default settings will be applied
-    public float jumpCooldownTimer; //How long they must wait before jumping again
-    public float horizontalSpeed; //How fast the character can move vertically
-    public float jumpPower; //How high the player can jump
+    public float jumpCooldownTimer = 2f; //How long they must wait before jumping again
+    public float horizontalSpeed = 5f; //How fast the character can move vertically
+    public float jumpPower = 20f; //How high the player can jump
     //Public Variables for Gun Mechanic
     //When the player fires the harpoon, we simply swap the arms out by making on active and the other inactive.
     public GameObject GunArm;
@@ -28,13 +27,6 @@ public class PlayerMovement : MonoBehaviour
         rb2d = GetComponent<Rigidbody2D>();
         animController = GetComponent<PlayerAnimationController>();
         harpoonController = GetComponent<HarpoonController>();
-        if (useDefaultOptions)
-        {
-            //CHANGE THESE TO INTERNAL VARIABLES!
-            jumpCooldownTimer = 2f;
-            horizontalSpeed = 750f;
-            jumpPower = 20f;
-        }
     }
 
     // Update is called once per frame
@@ -64,7 +56,7 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         //With ForceMode2D.Force, it requires a bigger value to get the results you want.
-        Vector2 hforce = new Vector2(650f * horizontal * Time.deltaTime, 0);
+        Vector2 hforce = new Vector2(300f * horizontalSpeed * horizontal * Time.fixedDeltaTime, 0);
         rb2d.AddForce(hforce, ForceMode2D.Force);
     }
 
